ff172522cf1bc62e39fc2aa10939b422b134b9e4
sgr-0007 <sagarhs0007@gmail.com>
2025-02-01 22:58:56 +0000

    iokit component

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import React from "react";
-import { ImageKitProvider, IKImage } from "imagekitio-next";
+import { ImageKitProvider } from "imagekitio-next";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
